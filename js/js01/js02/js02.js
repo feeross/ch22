649f02ed3be728ab0a1943f4f9417b0f49ce513b
Fernanda Rosales <fernanda.rosales.ortega@gmail.com>
2023-01-11 14:00:56 -0600

    Subir archivos de sesion 2

---
+++ js/js01/js02/js02.js
@@ -0,0 +1,16 @@
+console.log("Sesión JS02");
+
+/*
+function declaretion / function statment
+
+una caracteristicas de estas funciones es que tienen hoisting (elevacion).
+
+*/
+
+function product(a, b) {
+    return a * b;
+
+}
+console.log( ("El resultado de 5 * 10 = " + product(5,10) ) );
+
+
